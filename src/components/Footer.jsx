84eb69af093f83bee704cebc8435ff12f6801f25
Yodit Melile <96232166+yoditmelile@users.noreply.github.com>
2022-11-29 23:19:46 +0300

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/4sec.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Check out our other staffs <b>Four Second</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
